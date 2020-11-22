733d5c712e6d656cbf017811cc3ceb17e1491266
VLAD HORNAI <vlad@DESKTOP-SBSF56P.localdomain>
2020-11-22 19:40:07 +0200

    v1 of pyModbus client

--- pyModbus_Client.py
+++ pyModbus_Client.py
@@ -1,29 +1,24 @@
 
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus.client.sync import ModbusTcpClient
 import logging
 import json 
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-
-UNIT = 0x1
+import Modbus_Gw_Parsed
 
 # Opening JSON file 
 filename = 'modbus_gw_file_parsed.json'
 
 # Iterating through the json 
 # list 
-client = ModbusClient('127.0.0.1', port=502)
+client = ModbusTcpClient('127.0.0.1', port=502)
 client.connect()
 
 with open(filename, 'r') as file:
     data = json.load(file)
     for obj in data: 
-        #for key, value in obj.items():
         startaddr = int(obj.get("start_addr", None).strip(" "))
         wordcnt = int(obj.get("word_cnt",None))
-        rr = client.read_input_registers(startaddr, wordcnt, unit=UNIT)
-        print(rr)
-        #print(startaddr, type(wordcnt))
+        reg = client.read_input_registers(startaddr,wordcnt)
+        print(reg.registers)
+        
         
 client.close()
