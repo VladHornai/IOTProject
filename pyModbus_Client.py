2c9378b4abf615404b5f0d9ec5af6c5d36e486c9
VLAD HORNAI <vlad@DESKTOP-SBSF56P.localdomain>
2020-11-22 22:25:54 +0200

    version with output file json

--- pyModbus_Client.py
+++ pyModbus_Client.py
@@ -6,11 +6,12 @@ import Modbus_Gw_Parsed
 
 # Opening JSON file 
 filename = 'modbus_gw_file_parsed.json'
-
 # Iterating through the json 
 # list 
 client = ModbusTcpClient('127.0.0.1', port=502)
 client.connect()
+gw_dict = {}
+count = 0
 
 with open(filename, 'r') as file:
     data = json.load(file)
@@ -18,6 +19,18 @@ with open(filename, 'r') as file:
         startaddr = int(obj.get("start_addr", None).strip(" "))
         wordcnt = int(obj.get("word_cnt", None))
         reg = client.read_input_registers(startaddr,wordcnt)
-        print(reg.registers)
-            
+
+        r = {
+            "register": startaddr,
+            "response": reg.registers
+        }
+
+        gw_dict[count] = r
+        count += 1
+        #print(gw_dict)
+
+dataJSON = json.dumps(gw_dict, indent=4)    
+out_file = open("pyModbus.json", "w")
+out_file.write(dataJSON)
+out_file.close()  
 client.close()
