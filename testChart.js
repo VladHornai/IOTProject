762152c1e3bb805ebe63ecc2a4d6b14b458985b7
VladHornai <54432287+VladHornai@users.noreply.github.com>
2020-12-05 23:15:00 +0200

    Chart commit

--- testChart.js
+++ testChart.js
@@ -29,7 +29,7 @@ Promise.all([
 
           //we will work with data 2!
 }
-
+console.log("here")
 
 window.chartColors = { 	red: 'rgb(255, 99, 132)', 	orange: 'rgb(255, 159, 64)', 	yellow: 'rgb(255, 205, 86)', 	green: 'rgb(75, 192, 192)', 	blue: 'rgb(54, 162, 235)', 	purple: 'rgb(153, 102, 255)', 	grey: 'rgb(201, 203, 207)' }; 
 
